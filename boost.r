7b7dafa4f6170bbdc27d231f95805f9dc364afd3
Neel Mehta <neelmehta@college.harvard.edu>
2016-04-19 21:11:19 -0400

    Porting in new code from other branch

--- boost.r
+++ boost.r
@@ -52,7 +52,6 @@ candidate_score <- function(for_candidate, against_candidate) {
 # vector contains (White, Black, Hispanic, Asian)
 # in white-white elections, the democrat (arbitrarily chosen) is the minority
 minority_scores <- function(row) {
-
     # column names are like `R.White` and `D.Asian` so we have to prepend the
     # party label before the race
     for_party <- if (is_minority_democrat(row)) 'D.' else 'R.'
@@ -72,9 +71,16 @@ minority_scores <- function(row) {
 expected_democratic_scores <- function(year) {
     year_data <- preferences[preferences$Year == year,]
     if (nrow(year_data) > 0) {
-        # our csv stores percentage points (e.g. 45), we need to convert to percent (e.g. 0.45)
-        # TODO refactor to use candidate_score (similarly to above fn)
-        return (c(year_data$White.D., year_data$Black.D., year_data$Hispanic.D., year_data$Asian.D.) / 100)
+# for every race, get a generic democrat's score for that race
+        # we only track the democratic% since the data given to us
+        # have democratic% + republican% = 100%, so we can just subtract
+        # the D% from 100% to get R%
+        map_fn <- function(race){
+            for_candidate <- as.numeric(year_data[[p(race, '.D.')]])
+            against_candidate <- 100 - for_candidate
+            return (candidate_score(for_candidate, against_candidate))
+        }
+        return (capply(races, map_fn))
     }
     else {
         # this year isn't in the dataset but interpolate from the surrounding years
@@ -115,10 +121,7 @@ minority_boost <- function(row) {
 # Returns a table of racial boosts
 racial_boosts <- function() {
     # map boost over all candidates
-    boosts <- apply(candidates, 1, function(row) {
-      z <- noquote(row)
-      return (minority_boost(z))
-    })
+    boosts <- apply(candidates, 1, minority_boost)
 
     # get list of minorities per election
     minorities <- apply(candidates, 1, minority_race_of_election)
@@ -126,7 +129,7 @@ racial_boosts <- function() {
     # merge minority and boost data to get one big matrix
     # each row = candidate, W boost, B boost, H boost, A boost
     # note that this turns all the numbers into strings... can extract/numerify later
-    combined <- cbind(matrix(minorities),t(boosts))
+    combined <- cbind(matrix(minorities), t(boosts))
     colnames(combined) <- list("MinorityRace", "WhiteBoost", "BlackBoost", "HispanicBoost", "AsianBoost")
 
     # calculate the boosts for every race
